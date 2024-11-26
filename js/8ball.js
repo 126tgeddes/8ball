2010a90a1e9a7d832f54c95009f3e26ea1dd71c2
Timothy <126tgeddes@frhsd.com>
2024-11-26 12:49:29 -0500

    11/26/24

---
+++ js/8ball.js
@@ -0,0 +1,27 @@
+document.getElementById('ask-button').addEventListener('click',ask);
+
+
+function ask() {
+    const answerDiv = document.getElementById("answer");
+    document.getElementById("ask-button").classList.add('shake');
+    const randomIndex = Math.floor(Math.random() * responses.length);
+    setTimeout(showAnswer,2000)
+  }
+
+
+  function showAnswer(){
+    const randomIndex = Math.floor(Math.random() * responses.length);
+    let answerDiv=document.getElementById('answer');
+    answerDiv.textContent = responses[randomIndex];
+
+  }
+
+const responses = [
+    "Yes, definitely",
+    "It is certain",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful"
+  ];
+
+  
